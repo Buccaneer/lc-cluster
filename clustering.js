22502aef82c0dd3fc826f4e03770a71852d7231c
Pieter-Jan Vandenberghe <pieterjan.vandenberghe@outlook.com>
2019-05-08 15:58:16 +0200

    Util function for merge-based clustering

--- clustering.js
+++ clustering.js
@@ -3,6 +3,21 @@ const Fetcher = require('./fetcher.js'),
 
 module.exports = class Clustering {
     
+    static cluster(stops) {
+        // Turn stops object into array
+        clusters = Object.keys(stops).map(key => [stops[key]])
+        // TODO
+        this.constructor.utilFunction()
+    }
+    
+    // Calculates util value for clusters a and b
+    // A cluster is [stops]
+    static utilFunction(a, b) {
+        let wab = a.map(s => Object.keys(s.neighbours).filter(n => b.findIndex(e => e["@id"] == n) > -1).reduce((acc, n) => acc + s.neighbours[n].count, 0)).reduce((acc, val) => acc + val, 0) 
+        let wba = b.map(s => Object.keys(s.neighbours).filter(n => a.findIndex(e => e["@id"] == n) > -1).reduce((acc, n) => acc + s.neighbours[n].count, 0)).reduce((acc, val) => acc + val, 0) 
+        return 1 / a.length / b.length * (wab / Math.sqrt(a.length) + wba / Math.sqrt(b.length))
+    }
+    
     static createSummary(stopUrls, connectionUrls, startTime, stopTime) {
         return new Promise(async (resolve, reject) => {
             let summary = await Fetcher.fetchGraph(stopUrls)
